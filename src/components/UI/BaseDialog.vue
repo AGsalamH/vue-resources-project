88abf1e452d2ca3d923d6d60670715ef0e98a0a0
AGsalamH <AGsalamH@gmail.com>
2021-12-20 16:59:25 +0200

    teleport dialog component to body

--- src/components/UI/BaseDialog.vue
+++ src/components/UI/BaseDialog.vue
@@ -1,17 +1,19 @@
 <template>
-	<dialog open>
-		<header>
-			<slot name="header"> {{ title }} </slot>
-		</header>
-		<section>
-			<slot> </slot>
-		</section>
-		<menu>
-			<slot name="actions">
-				<base-button @click="$emit('close')">Close</base-button>
-			</slot>
-		</menu>
-	</dialog>
+	<teleport to="body">
+		<dialog open>
+			<header>
+				<slot name="header"> {{ title }} </slot>
+			</header>
+			<section>
+				<slot> </slot>
+			</section>
+			<menu>
+				<slot name="actions">
+					<base-button @click="$emit('close')">Close</base-button>
+				</slot>
+			</menu>
+		</dialog>
+	</teleport>
 </template>
 
 <script>
